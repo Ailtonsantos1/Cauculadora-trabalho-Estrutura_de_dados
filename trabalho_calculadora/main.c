e5b434c312a7052de131f3831fc8765d290c98a5
Antonio Vinicius Alvino Silva <antonio.alvino@a.ucb.br>
2025-06-19 20:34:19 -0300

    Refactor: Move MAX_LINE definition to the top of main.c

--- trabalho_calculadora/main.c
+++ trabalho_calculadora/main.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include "expressao.h"
 
+#define MAX_LINE 256
+
 int main() {
     Expressao expressoes[100];
     char linha[512];
@@ -12,23 +14,19 @@ int main() {
         printf("Erro ao abrir o arquivo 'entradas.txt'\n");
         return 1;
     }
-char linha[MAX_LINE];
+
     int linhaAtual = 1;
 
-    while (fgets(linha, sizeof(linha), arquivo)) {
-        char copia[MAX_LINE];
+
+    while (fgets(linha, sizeof(linha), f) && i < 100) {
+
+char copia[MAX_LINE];
         strcpy(copia, linha);
 
         if (verificaPosFixada(copia)) {
             printf("Linha %d: POS-FIXADA VÁLIDA ✅\n", linhaAtual);
-        } else {
-            printf("Linha %d: INVÁLIDA ❌\n", linhaAtual);
-        }
+            
 
-        linhaAtual++;
-    }
-
-    while (fgets(linha, sizeof(linha), f) && i < 100) {
         linha[strcspn(linha, "\n")] = '\0';
         strcpy(expressoes[i].posFixa, linha);
         strcpy(expressoes[i].inFixa, getFormaInFixa(expressoes[i].posFixa));
@@ -41,6 +39,12 @@ char linha[MAX_LINE];
         printf("--------------------------\n");
 
         i++;
+        } else {
+            printf("Linha %d: INVÁLIDA ❌\n", linhaAtual);
+        }
+
+        linhaAtual++;
+
     }
 
     fclose(f);
